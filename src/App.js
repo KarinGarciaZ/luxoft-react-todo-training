9117716ae8ca67e72c476d54b820d894481d6f8e
Oscar Garcia <v-oscgarcia@expedia.com>
2020-05-15 22:23:57 -0500

    use reducer implementation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React, {useState, useCallback} from 'react';
+import React, {useCallback, useReducer} from 'react';
 import './App.css';
 import Input from './components/input';
 import Item from './components/item';
 
+const initialState = [];
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'add':
+      return [ ...state, action.payload];
+    case 'remove':
+      const todoListCopy = [ ...state ];
+      const indexToComplete = todoListCopy.findIndex( item => item.id === action.payload );
+      todoListCopy.splice(indexToComplete, 1);
+
+      return todoListCopy;
+    case 'complete':
+      const listCopy = [ ...state ];
+      const indexComplete = listCopy.findIndex( item => item.id === action.payload );
+      listCopy[indexComplete].completed = true;
+
+      return listCopy;
+    default:
+      throw new Error();
+  }
+}
+
 const App = () => {
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, dispatch] = useReducer(reducer, initialState);
 
   const addItem = useCallback( textValue => {
     const newItem = { 
@@ -12,23 +35,17 @@ const App = () => {
       value: textValue, 
       completed: false 
     };
-    setTodoList(currentList => [ ...currentList, newItem ]);
+
+    dispatch({type: 'add', payload: newItem});
   }, []);
 
   const removeItem = (id, e) => {
     e.stopPropagation();
-
-    const todoListCopy = [ ...todoList ];
-    const indexToComplete = todoListCopy.findIndex( item => item.id === id );
-    todoListCopy.splice(indexToComplete, 1);
-    setTodoList(todoListCopy);
+    dispatch({type: 'remove', payload: id});
   };
 
   const completeItem = id => {
-    const todoListCopy = [ ...todoList ];
-    const indexToComplete = todoListCopy.findIndex( item => item.id === id );
-    todoListCopy[indexToComplete].completed = !todoListCopy[indexToComplete].completed;
-    setTodoList(todoListCopy);
+    dispatch({type: 'complete', payload: id});
   };
 
   const itemsToLoad = todoList.map( item => {
